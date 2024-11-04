282d87cd92a29131472326c4826e47d667540ed1
Michael <Dahl>
2024-11-04 10:42:36 -0800

    removed unnecessary folders/files

--- GHFollowers/Model/User.swift
+++ GHFollowers/Model/User.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+public struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var createdAt: String
+    var followers: Int
+    var following: Int
+}
