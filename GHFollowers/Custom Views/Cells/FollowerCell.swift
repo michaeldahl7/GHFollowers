db92e7b01196f0d0f1684031be57075c7bb4f30b
Michael <Dahl>
2024-11-05 12:52:09 -0800

    Collection view displayed

---
+++ GHFollowers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,12 @@
+//
+//  FollowerCell.swift
+//  GHFollowers
+//
+//  Created by Michael Dahl on 11/4/24.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+}
---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,20 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Michael Dahl on 11/4/24.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
